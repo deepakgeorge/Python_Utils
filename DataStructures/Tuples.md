c015d7c3c86c7476e61c0308d10fe9c789b5a954
deepakiim <deepakwithyou@gmail.com>
2018-10-15 09:19:23 +0530

    added List.ipynb

--- DataStructures/Tuples.md
+++
@@ -1,4 +0,0 @@
-* Sort list of tuples
-  ```
-  sorted([('abc', 121),('abc', 231),('abc', 148), ('abc',221)], key=lambda x: x[1])
-  ```
--- Matplotlib/scatter_plot_r1c1.py
+++ Matplotlib/scatter_plot_r1c1.py
@@ -1,9 +1,9 @@
 
 fig, ax = plt.subplots()
 #ax.set_title('RMS & Kurtosis visualization')
-ax.scatter(lag_base_test_predict['b1_vib_kurt_hzrate'],lag_base_test_predict['life_per'], marker= 'o',alpha=0.5, lw=0.3,label='life_per')
+ax.scatter(df['b1_vib_kurt_hzrate'],df['life_per'], marker= 'o',alpha=0.5, lw=0.3,label='life_per')
 ax.set_ylabel('life_per')
-ax.scatter(lag_base_test_predict['b1_vib_kurt_hzrate'],lag_base_test_predict['y_pred'], marker= 'o',alpha=0.5, lw=0.3,label='y_pred')
+ax.scatter(df['b1_vib_kurt_hzrate'],df['y_pred'], marker= 'o',alpha=0.5, lw=0.3,label='y_pred')
 ax.set_xlabel('b1_vib_kurt_hzrate')
 ax.legend(bbox_to_anchor=(0.4,-0.10), loc=3,ncol=10,borderaxespad=0, edgecolor='k').draggable() #ncol makes legend horizontal. give high value like 10 when not sure of number of legends
 fig.savefig('../docs/plots/b1_vib_kurt_hzrate_ypred_lm.png', bbox_inches='tight')
