8bb37589a56dab71337824e0c4c38d579b05d2a2
deepakiim <deepakwithyou@gmail.com>
2018-10-19 11:14:01 +0530

    added EDP training material

--- Pandas/pandas_date_diff_minutes.py
+++ Pandas/pandas_date_diff_minutes.py
@@ -1,3 +1,3 @@
 #https://pandas.pydata.org/pandas-docs/stable/timedeltas.html#frequency-conversion
 #Converting difference in dates to mins
-basic_ft_base['age'] = (basic_ft_base['read_time'] - min(basic_ft_base['read_time']))/np.timedelta64(1, 'm')
+df['age'] = (df['read_time'] - min(df['read_time']))/np.timedelta64(1, 'm')
