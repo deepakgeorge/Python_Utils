32c0f25cecfff9282877acc61f3052fed1b91b5a
deepak <deepakwithyou@gmail.com>
2019-02-28 00:18:43 +0530

    renamed filenames with caps to lowercase

---
+++ matplotlib/scatter_plot_r1c1.py
@@ -0,0 +1,10 @@
+
+fig, ax = plt.subplots()
+#ax.set_title('RMS & Kurtosis visualization')
+ax.scatter(df['b1_vib_kurt_hzrate'],df['life_per'], marker= 'o',alpha=0.5, lw=0.3,label='life_per')
+ax.set_ylabel('life_per')
+ax.scatter(df['b1_vib_kurt_hzrate'],df['y_pred'], marker= 'o',alpha=0.5, lw=0.3,label='y_pred')
+ax.set_xlabel('b1_vib_kurt_hzrate')
+# IMP use label argument in ax.scatter() or ax.plot() if labels are not coming up in the plot
+ax.legend(bbox_to_anchor=(0.4,-0.10), loc=3,ncol=10,borderaxespad=0, edgecolor='k').draggable() #ncol makes legend horizontal. give high value like 10 when not sure of number of legends
+fig.savefig('../docs/plots/b1_vib_kurt_hzrate_ypred_lm.png', bbox_inches='tight')
